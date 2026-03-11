755ab9047eb6f8b85b042f9c18df6457a869d32d
artesarh <rbhtn@icloud.com>
2026-03-11 20:35:06 +0000

    fix(hotkeys): Fixed C-s to save file

--- init.lua
+++ init.lua
@@ -206,6 +206,9 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
+-- Save file with Ctrl+S
+vim.keymap.set({ 'n', 'i' }, '<C-s>', '<cmd>write<CR><Esc>', { desc = 'Save file' })
+
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
